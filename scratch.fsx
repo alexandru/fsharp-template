45fb32d7fc6249cf935e175735bdaa2d7523316c
Alexandru Nedelcu <noreply@alexn.org>
2019-08-17 18:23:36 +0300

    Formatting

--- scratch.fsx
+++ scratch.fsx
@@ -1,8 +1,7 @@
 // The result of: paket generate-load-scripts
-#load ".paket/load/netstandard2.0/Newtonsoft.Json.fsx"
-#load "src/Library/Library.fs"
-
-open System
+#load "./.paket/load/netstandard2.0/Newtonsoft.Json.fsx"
+#load "./src/Library/Library.fs"
+open FSharp.Core
 open Library
 
 // ...
