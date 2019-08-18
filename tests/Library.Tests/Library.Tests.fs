0753ab01926229e81da7759a19e553c598cb8ce1
Alexandru Nedelcu <noreply@alexn.org>
2019-08-18 08:13:39 +0300

    Fix tests

---
+++ tests/Library.Tests/Library.Tests.fs
@@ -0,0 +1,26 @@
+namespace Library.Tests
+
+module Tests =
+    open Expecto
+
+    [<Tests>]
+    let tests = testList "samples" [
+        testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
+            let subject = true
+            Expect.isTrue subject "I compute, therefore I am."
+
+        testCase "I'm skipped (should skip)" <| fun _ ->
+            Tests.skiptest "Yup, waiting for a sunny day..."
+
+        testCase "contains things" <| fun _ ->
+            Expect.containsAll
+                [| 2; 3; 4 |] [| 2; 4 |]
+                "This is the case; {2,3,4} contains {2,4}"
+
+        testCase "Sometimes I want to ༼ノಠل͟ಠ༽ノ ︵ ┻━┻" <| fun _ ->
+            Expect.equal "abcdëf" "abcdëf" "These should equal"
+
+        test "I am (should fail)" {
+            "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true true
+        }
+    ]
